8b463ea1634ec1d6a1be4f69841a0424152a4de0
Arnaud-Dlt <arnauddeletre@outlook.fr>
2023-01-12 11:58:37 +0000

    13H

--- Projet_Pizza/assets/js/classes/ingredient.js
+++ Projet_Pizza/assets/js/classes/ingredient.js
@@ -24,3 +24,5 @@ class Ingredient {
     }
     
 }
+
+export {Ingredient};
--- Projet_Pizza/assets/js/index.js
+++ Projet_Pizza/assets/js/index.js
@@ -1,23 +1,67 @@
-import {Ingredient} from './ingredient.js'
+import { Ingredient } from './classes/ingredient.js'
 
-window.addEventListener("DOMContentLoaded", function (){
+window.addEventListener("DOMContentLoaded", function() {
+    let bacon = new Ingredient("Bacon", "assets/img/bacon.png");
+    let carots = new Ingredient("Carotte", "assets/img/carrots.png");
+    let cheese = new Ingredient("Fromage", "assets/img/cheese.png");
+    let egg = new Ingredient("Oeuf", "assets/img/egg.png");
+    let eggplant = new Ingredient("Aubergine", "assets/img/eggplant.png");
+    let goat_cheese = new Ingredient("Fromage de chèvre", "assets/img/goat-cheese.png");
+    let honey = new Ingredient("Miel", "assets/img/honey.png");
+    let mushroom = new Ingredient("Champignon", "assets/img/mushroom.png");
+    let olive = new Ingredient("Olive", "assets/img/olive.png");
+    let pepper = new Ingredient("Piment", "assets/img/pepper.png");
+    let potato = new Ingredient("Pomme de terre", "assets/img/potato.png");
+    let tomato = new Ingredient("Tomate", "assets/img/tomato.png");
+
+    let availableIngredients = [];
+
+    availableIngredients.push(bacon, carots, cheese, egg, eggplant, goat_cheese, honey, mushroom, olive, pepper, potato, tomato);
+
+    console.log(availableIngredients);
+
+    let section = document.getElementById("stage");
+    let ul = document.createElement("ul");
+    section.appendChild(ul);
     
-        let availableIngredients=[];
-        let bacon = new Ingredient("assets/img/bacon.png");
-        let carots = new Ingredient("assets/img/carrots.png");
-        let cheese = new Ingredient("assets/img/cheese.png");
-        let egg = new Ingredient("assets/img/egg.png");
-        let eggplant = new Ingredient("assets/img/eggplant.png");
-        let goat_cheese = new Ingredient("assets/img/goat-cheese.png");
-        let honey = new Ingredient("assets/img/honey.png");
-        let mushroom = new Ingredient("assets/img/mushroom.png");
-        let olive = new Ingredient("assets/img/olive.png");
-        let pepper = new Ingredient("assets/img/pepper.png");
-        let potato = new Ingredient("assets/img/potato.png");
-        let tomato = new Ingredient("assets/img/tomato.png");
+    for (let i = 0; i < availableIngredients.length; i++) {
+
+        let li = document.createElement("li");
+        let article = document.createElement("article");
+        let header = document.createElement("header");
+        let figure = document.createElement("figure");
+        let img = document.createElement("img");
+        let h3 = document.createElement("h3");
+        let h3text = document.createTextNode(availableIngredients[i].name);
+
+        // Création des balises
+        ul.appendChild(li);
+        li.appendChild(article);
+        article.appendChild(header);
+        header.appendChild(figure);
+        header.appendChild(h3);
+        figure.appendChild(img);
+
+        // Ajout attribute
+        img.setAttribute("src", availableIngredients[i].file);
+        img.setAttribute("alt", availableIngredients[i].name);
         
-        availableIngredients.push(bacon,carots,cheese,egg,eggplant,goat_cheese,honey,mushroom,olive,pepper,potato,tomato);
+        article.setAttribute("class", "not:selected");
         
-        console.log(availableIngredients);
+        // Ajout Nom Ingrédient
+        h3.appendChild(h3text);
+    }
+    
+    let selectIngredient = document.querySelectorAll("#stage article");
+    
+    selectIngredient.addEventListener("click", function() {
+        for (ingredient of selectIngredient) {
+            if (ingredient.classList.contains()) {
+    
+                ingredient.classList.toggle("selected");
+    
+            }
+        }
+    });
     
 })
